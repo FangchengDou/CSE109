8ef38ea125f64b0e3ef3bb174accd147341ecc2c
DFC <frankdoufc@gmail.com>
2022-04-02 13:23:09 -0700

    1

---
+++ src/set0/POJ_1917.java
@@ -0,0 +1,19 @@
+package set0;
+
+import java.util.*;
+
+public class POJ_1917 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String output = "";
+        int n = Integer.parseInt(in.nextLine());
+        for (int i = 0; i < n; i++) {
+            String l1 = in.nextLine();
+            String l2 = in.nextLine();
+            String[] s = l1.split("[<>]");
+            output += s[0] + s[1] + s[2] + s[3] + s[4] + "\n";
+            output += l2.replaceAll("\\...", s[3] + s[2] + s[1] + s[4]) + "\n";
+        }
+        System.out.print(output);
+    }
+}
