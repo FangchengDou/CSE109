03bddb2f004c5bc3c21b6b211d02e99bfb82d391
DFC <frankdoufc@gmail.com>
2022-04-02 20:41:23 -0700

    1

--- src/set0/POJ_2719.java
+++ src/set0/POJ_2719.java
@@ -5,18 +5,17 @@ import java.util.*;
 public class POJ_2719 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        String output = "";
-        int n = in.nextInt();
-        while (n != 0) {
-            String n1 = Integer.toString(n);
-            n1 = n1.replace('5', '4');
-            n1 = n1.replace('6', '5');
-            n1 = n1.replace('7', '6');
-            n1 = n1.replace('8', '7');
-            n1 = n1.replace('9', '8');
-            output += n + ": " + Integer.parseInt(n1, 9) + "\n";
-            n = in.nextInt();
+        String n = in.nextLine();
+        while (!n.equals("0")) {
+            System.out.print(n);
+            n = n.replace('5', '4');
+            n = n.replace('6', '5');
+            n = n.replace('7', '6');
+            n = n.replace('8', '7');
+            n = n.replace('9', '8');
+            System.out.print(": " + Integer.parseInt(n, 9) + "\n");
+            n = in.nextLine();
         }
-        System.out.print(output);
+
     }
 }
